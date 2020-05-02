678317fca40798c412b29223d3096c46ed67cf25
MetalStorm15 <metalstorm15@163.com>
2020-05-02 12:06:32 +0800

    修改路径中文名称

--- HeadFirstC/Chapter1/notepad.c
+++ HeadFirstC/Chapter1/notepad.c
@@ -4,16 +4,19 @@
  * (c)2020, The College Blackjack Team.
  */
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
 {
     int decks;
     puts("Enter a number of decks");
     scanf("%i", &decks);
-    if (deck < 1) {
-        puts("That is not a valid numbers of decks");
+    if (decks < 1) {
+        puts("That is not a valid numbers of decks\n");
         return 1;
     }
+
     printf("There are %i cards\n", (decks * 52));
+	/*暂停一下，用于观察运行结果*/
+	
     return 0;
 }
